1786c8e8a2da3690cf6443e505d1ecda675c680f
Rafael Rozon <rafael.rozon@colonynetworks.com>
2017-05-06 21:38:15 -0700

    dist files

--- dist/styles.js
+++ dist/styles.js
@@ -1,5 +1,11 @@
 'use strict';
 
+var _defineProperty2 = require('babel-runtime/helpers/defineProperty');
+
+var _defineProperty3 = _interopRequireDefault(_defineProperty2);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 module.exports = {
     log: {
         background: 'url(error.svg)',
@@ -37,22 +43,17 @@ module.exports = {
         display: 'inline-block',
         margin: '0 15px 0 0'
     },
-    clearBtn: {
-        background: 'rgba(255, 255, 255, 0.498039)',
-        borderBottom: 'none',
-        borderImage: 'initial',
-        borderLeft: '1px solid rgba(0, 0, 0, 0.2)',
-        borderRight: 'none',
-        borderRadius: '4px 0px 0px',
-        borderTop: '1px solid rgba(0, 0, 0, 0.2)',
+    clearBtn: (0, _defineProperty3.default)({
+        background: 'none',
+        border: 'none',
         bottom: '0px',
         color: 'orange',
         fontSize: '.9em',
         outline: 'none',
         padding: '5px 10px',
-        position: 'absolute',
-        right: '0px'
-    },
+        position: 'fixed',
+        right: '.8em'
+    }, 'bottom', '.8em'),
     logIcon: {
         display: 'inline-block',
         height: '1em',
